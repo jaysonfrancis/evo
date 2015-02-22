2eb10589a382b6378f786efb34e49fc36763680d
Aaron J Blancaflor <aaron.j.blancaflor@gmail.com>
2015-02-22 15:08:25 -0800

    <added isr.c, proc.c and proc.h and modified to work with trapframe>

---
+++ phase1/UseTrapframe/isr.c
@@ -0,0 +1,67 @@
+// isr.c, 159
+#include "spede.h"
+#include "type.h"
+#include "isr.h"
+#include "tool.h"
+#include "extern.h"
+#include "proc.h"
+
+void CreateISR(int pid) {
+ //  if pid given is not 0 (Idle), enqueue it into run queue
+   if (pid != 0) EnQ(pid, &run_q);
+
+ //  PCB of new proc:
+    MyBZero(&pcb[pid], sizeof(pcb_t));   
+
+    pcb[pid].mode = UMODE; // Mode is set to UMODE
+    pcb[pid].state = RUN; // State is set to RUN
+    pcb[pid].runtime = 0;
+    pcb[pid].total_runtime = 0; // Both Runtime counts are reset to 0
+	//Added in trapFrame section
+	
+	
+	// ****************************************************
+	// besides code in old CreateISR() still included, add:
+	// ****************************************************
+
+	MyBzero(stack[pid], STACK_SIZE); // erase stack
+
+	// point to just above stack, then drop by sizeof(TF_t)
+	pcb[pid].TF_ptr = (TF_t *)&stack[pid][STACK_SIZE];
+	pcb[pid].TF_ptr--;
+
+	// fill out trapframe of this new proc:
+	if(pid == 0)
+      pcb[pid].TF_ptr->eip = (unsigned int)Idle; // Idle process
+	else
+      pcb[pid].TF_ptr->eip = (unsigned int)UserProc; // other new process
+
+	pcb[pid].TF_ptr->eflags = EF_DEFAULT_VALUE | EF_INTR;
+	pcb[pid].TF_ptr->cs = get_cs();
+	pcb[pid].TF_ptr->ds = get_ds();
+	pcb[pid].TF_ptr->es = get_es();
+	pcb[pid].TF_ptr->fs = get_fs();
+	pcb[pid].TF_ptr->gs = get_gs();
+}
+
+
+void TerminateISR() {
+   if(CRP < 1) return;//just return if CRP is 0 or -1 (Idle or not given)
+      
+   pcb[CRP].state = NONE;//change state of CRP to NONE
+   EnQ(CRP,&none_q);//queue it to none queue
+   CRP = -1;//set CRP to -1 (none)
+}        
+
+void TimerISR() {
+        if(CRP<=0) return; //just return if CRP is Idle (0) or less (-1)
+        pcb[CRP].runtime++;//upcount the runtime of CRP
+        if (pcb[CRP].runtime == TIME_LIMIT){ //if the runtime of CRP reaches TIME_LIMIT
+	 // ??//(need to rotate to next PID in run queue)
+	pcb[CRP].total_runtime += pcb[CRP].runtime;//sum up runtime to the total runtime of CRP
+	pcb[CRP].state=RUN;//change its state to RUN
+	EnQ(CRP,&run_q);//queue it to run queue
+	CRP=-1; //reset CRP (to -1, means none)     
+	   }
+       
+}
---
+++ phase1/UseTrapframe/proc.h
@@ -0,0 +1,10 @@
+// proc.h, 159
+
+#ifndef _PROC_H_
+#define _PROC_H_
+
+//Got rid of Dispatch() for use in trapframe
+void Idle();
+void UserProc();
+
+#endif
