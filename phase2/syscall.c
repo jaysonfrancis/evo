5131dfcf7e8dc9eaee87a2c12e59c997a56754c6
Aaron J Blancaflor <aaron.j.blancaflor@gmail.com>
2015-02-23 17:19:26 -0800

    <commited>

--- phase2/syscall.c
+++ phase2/syscall.c
@@ -3,23 +3,25 @@
 
 #include "syscall.h" //prototype these below
 
-int GetPid(){
+int GetPid(){ //programed to entry 48 in IDT
 	int pid;
 	
 	//assembly code initialization
 	asm("int $48; movl %%ebx, %0" //CPU inst
-	:"g" (pid) // output line for asm().
+	:"=g" (pid) // output line for asm().
 	:          // no input into asm()
 	:"%ebx");  // push/pop before/after asm()
 	
 	return pid;
 }
 
-void Sleep(int sec){
-	//??? need to complete the code
-	asm("movl %0, %%ebx,int $49"
-	: //output line for asm()
+void Sleep(int sec){ //coded to entry 49 in IDT
+	/* TimerEntry in entry.S
+	// must push 32, then 48, then 49
+	*/
+	asm("movl %0, %%ebx; int $49"
+	: //no output line for asm()
 	:"g" (sec) //input line for asm()
-	:"ebx" // push/pop before/after asm()
+	:"%ebx" // what register to push/pop before/after asm()
 	);
 }
