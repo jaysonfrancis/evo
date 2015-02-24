cf8cc924e595047ede04660172e0a43525d7ca0b
Aaron J Blancaflor <aaron.j.blancaflor@gmail.com>
2015-02-23 17:29:57 -0800

    <copying everything from pure trapframe>

---
+++ phase2/proc.c
@@ -0,0 +1,29 @@
+// proc.c, 159
+// processes are here
+
+#include "spede.h"   // for IO_DELAY() needed here below
+#include "extern.h"  // for current_run_pid needed here below
+#include "proc.h"    // for Idle, SimpleProc, DispatchProc
+
+//Got rid of dispatch for use in trapframe
+
+void Idle() {
+	for(;;){
+		int i; // We added
+		// cons_putchar('0');
+		cons_printf("0 "); // print 0 ON PC?
+		for (i=0; i<1666000; i++) IO_DELAY(); // busy-loop delay for about 1 sec
+	} //end infinite loop
+}
+
+void UserProc() {
+	for(;;){
+		int i; // We added
+		cons_printf("%d ", CRP); // print its pid (CRP) on PC
+		for (i=0; i<1666000; i++) IO_DELAY(); // busy-loop delay for about 1 sec
+	}//end infinite loop
+}
+
+
+
+
