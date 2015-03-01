5be3cb8bb4662dba2848bb0d6e98cfb66f09dfaa
Jayson <jaysonfrancis@gmail.com>
2015-03-01 14:38:50 -0800

    commit

---
+++ samplephase3/semaphores/entry.h
@@ -0,0 +1,36 @@
+// entry.h, 159
+
+#ifndef _ENTRY_H_
+#define _ENTRY_H_
+
+#include <spede/machine/pic.h>
+
+#define TIMER_INTR 32
+#define GETPID_INTR 48
+#define SLEEP_INTR 49
+#define SEMWAIT_INTR 50
+#define SEMPOST_INTR 51
+
+#define KCODE 0x08    // kernel's code segment
+#define KDATA 0x10    // kernel's data segment
+#define KSTACK_SIZE 8192  // kernel's stack size, in chars
+
+// ISR Entries
+#ifndef ASSEMBLER
+
+__BEGIN_DECLS
+
+#include "type.h" // for trapframe_t below
+
+extern void TimerEntry();     // code defined in entry.S
+extern void Dispatch(TF_t *); // code defined in entry.S
+extern void GetPidEntry();    // code for entry getpid
+extern void SleepEntry();
+extern void SemWaitEntry();
+extern void SemPostEntry();
+
+__END_DECLS
+
+#endif
+
+#endif
---
+++ samplephase3/semaphores/syscall.c
@@ -0,0 +1,30 @@
+// syscall.c
+// collection of syscalls, i.e., API
+
+#include "syscall.h" // prototype these below
+
+int GetPid() {
+   int pid;
+
+   asm("int $48; movl %%ebx, %0" // CPU inst
+       : "=g"  (pid)             // 1 output from asm() 
+       :                         // no input into asm()
+       : "%ebx");                // push/pop before/after asm()
+    
+   return pid;
+}
+
+void Sleep(int sec) {
+    asm("movl %0, %%ebx ;int $49"
+        :
+        :"g" (sec)
+        :"%ebx");
+}
+
+void SemWait(){
+   
+}
+
+void SemPost(){
+   
+}
