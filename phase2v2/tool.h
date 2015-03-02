db4906dc08941cb614cd4041ceae3f1663c4887b
ablanca4 <aaron.j.blancaflor@gmail.com>
2015-03-02 15:50:58 -0800

    <added a v2 of phase2>

---
+++ phase2v2/tool.h
@@ -0,0 +1,15 @@
+// tool.h, 159
+// Team Evo
+
+#ifndef _TOOL_H
+#define _TOOL_H
+
+#include "type.h" // q_t needs be defined in code below
+
+void MyBZero(q_t *, int);
+void MyBzero(void  *, int );
+int DeQ(q_t *);
+void EnQ(int, q_t *);
+
+#endif
+
